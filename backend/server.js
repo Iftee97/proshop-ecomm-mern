e5a5a093457c18a636e619c54f4e72f3b03672f6
Iftee97 <tawsifmasrur@gmail.com>
2023-07-03 03:26:56 +0600

    added products router and controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { products } from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,26 +11,26 @@ const PORT = process.env.PORT || 4000;
 // connect to database
 connectDB();
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // routes
+app.use('/api/products', productRoutes);
+
+// home route
 app.get('/', (req, res) => {
   res.json({ message: 'API is up and running' });
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
+// error middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 // start server 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
 
 /*
   add to package.json maybe:
