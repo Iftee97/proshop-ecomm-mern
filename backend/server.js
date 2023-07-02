7ea298a894a2c1bdc8067143d649851d38138099
Iftee97 <tawsifmasrur@gmail.com>
2023-07-03 02:05:25 +0600

    added express backend and implemented data fetching in frontend from backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { products } from './data/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+// routes
+app.get('/', (req, res) => {
+  res.json({ message: 'API is up and running' });
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+
+// start server 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+/*
+  add to package.json maybe:
+  "build": "npm install && npm install --prefix frontend && npm run build --prefix frontend"
+*/
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,79 +1,96 @@
 import { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Spinner } from 'react-bootstrap'
 import { BsChevronLeft } from 'react-icons/bs'
 import Rating from '../components/Rating'
-import { products } from '../products'
+import axios from 'axios'
 
 export default function ProductScreen() {
+  const [product, setProduct] = useState({})
   const { id: productId } = useParams()
-  const { image, name, rating, numReviews, price, description, countInStock } = products.find(product => product._id === productId)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [productId])
+
+  console.log('product: >>>>>>>>>', product)
+  const { image, name, rating, numReviews, price, description, countInStock } = product
 
   return (
     <>
-      <Link to='/' className='btn btn-light my-3 inline-block'>
-        <span className='d-flex align-items-center'>
-          <BsChevronLeft />
-          <span className='ms-1'>
-            Go Back
-          </span>
-        </span>
-      </Link>
-      <Row>
-        <Col md={5}>
-          <Image src={image} alt={name} fluid />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h3>{name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating value={rating} text={`${numReviews} reviews`} />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Price: ${price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Description: {description}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>
-                      {countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                    </strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  type='button'
-                  variant="dark"
-                  className='btn btn-dark'
-                  disabled={countInStock === 0}
-                >
-                  Add to Cart
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+      {product ? (
+        <>
+          <Link to='/' className='btn btn-light my-3 inline-block'>
+            <span className='d-flex align-items-center'>
+              <BsChevronLeft />
+              <span className='ms-1'>
+                Go Back
+              </span>
+            </span>
+          </Link>
+          <Row>
+            <Col md={5}>
+              <Image src={image} alt={name} fluid />
+            </Col>
+            <Col md={4}>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <h3>{name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating value={rating} text={`${numReviews} reviews`} />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Price: ${price}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Description: {description}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>${price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        <strong>
+                          {countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                        </strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      type='button'
+                      variant="dark"
+                      className='btn btn-dark'
+                      disabled={countInStock === 0}
+                    >
+                      Add to Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </>
+      ) : (
+        <Spinner animation="border" variant="dark" />
+      )}
     </>
   )
 }
