98d3f9ed6ca59190b4d8d104d6d0f037dca8d06a
Iftee97 <tawsifmasrur@gmail.com>
2023-07-03 02:06:55 +0600

    added express backend and implemented data fetching in frontend from backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -22,75 +22,69 @@ export default function ProductScreen() {
 
   return (
     <>
-      {product ? (
-        <>
-          <Link to='/' className='btn btn-light my-3 inline-block'>
-            <span className='d-flex align-items-center'>
-              <BsChevronLeft />
-              <span className='ms-1'>
-                Go Back
-              </span>
-            </span>
-          </Link>
-          <Row>
-            <Col md={5}>
-              <Image src={image} alt={name} fluid />
-            </Col>
-            <Col md={4}>
-              <ListGroup variant='flush'>
-                <ListGroup.Item>
-                  <h3>{name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Rating value={rating} text={`${numReviews} reviews`} />
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  Price: ${price}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  Description: {description}
-                </ListGroup.Item>
-              </ListGroup>
-            </Col>
-            <Col md={3}>
-              <Card>
-                <ListGroup variant='flush'>
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Price:</Col>
-                      <Col>
-                        <strong>${price}</strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Status:</Col>
-                      <Col>
-                        <strong>
-                          {countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                        </strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Button
-                      type='button'
-                      variant="dark"
-                      className='btn btn-dark'
-                      disabled={countInStock === 0}
-                    >
-                      Add to Cart
-                    </Button>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card>
-            </Col>
-          </Row>
-        </>
-      ) : (
-        <Spinner animation="border" variant="dark" />
-      )}
+      <Link to='/' className='btn btn-light my-3 inline-block'>
+        <span className='d-flex align-items-center'>
+          <BsChevronLeft />
+          <span className='ms-1'>
+            Go Back
+          </span>
+        </span>
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={image} alt={name} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={rating} text={`${numReviews} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price: ${price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Description: {description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type='button'
+                  variant="dark"
+                  className='btn btn-dark'
+                  disabled={countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }
