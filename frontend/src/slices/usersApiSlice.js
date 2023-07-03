34cd7ce0841fbe2e4548acaab3fdac3976fb27af
Iftee97 <tawsifmasrur@gmail.com>
2023-07-03 17:10:06 +0600

    added authSliceand usersApiSlice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,87 @@
+import { apiSlice } from './apiSlice'
+import { USERS_URL } from '../constants'
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: builder => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: 'POST',
+      }),
+    }),
+    updateProfile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: 'PUT',
+        body: data,
+      }),
+    }),
+    // // we don't need a getUserDetails query because we're already storing the user info in the auth slice
+    // getUserDetails: builder.query({
+    //   query: () => ({
+    //     url: `${USERS_URL}/profile`,
+    //     method: 'GET',
+    //   }),
+    //   keepUnusedDataFor: 5,
+    // }),
+
+    // admin endpoints
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL,
+        method: 'GET',
+      }),
+      providesTags: ['User'],
+      keepUnusedDataFor: 5,
+    }),
+    getSingleUser: builder.query({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: 'GET',
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/${data.userId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['User'],
+    }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['User'],
+    }),
+  }),
+})
+
+export const {
+  useLoginMutation,
+  useRegisterMutation,
+  useLogoutMutation,
+  useUpdateProfileMutation,
+
+  // admin endpoints hooks:
+  useGetUsersQuery,
+  useGetSingleUserQuery,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} = usersApiSlice
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './slices/apiSlice'
 import { cartReducer } from './slices/cartSlice'
+import { authReducer } from './slices/authSlice'
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartReducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(apiSlice.middleware)
