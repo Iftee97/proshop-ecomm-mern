60d21bd3216b628fbeed95240b064e65c5eecde4
Iftee97 <tawsifmasrur@gmail.com>
2023-07-04 08:00:05 +0600

    ...

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,13 +1,13 @@
 // // dev urls (uncomment when running locally)
-export const BASE_URL = ''
-export const PRODUCTS_URL = '/api/products'
-export const USERS_URL = '/api/users'
-export const ORDERS_URL = '/api/orders'
-export const PAYPAL_URL = '/api/config/paypal'
+// export const BASE_URL = ''
+// export const PRODUCTS_URL = '/api/products'
+// export const USERS_URL = '/api/users'
+// export const ORDERS_URL = '/api/orders'
+// export const PAYPAL_URL = '/api/config/paypal'
 
 // // prod urls (comment out when running locally):
-// export const BASE_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}`
-// export const PRODUCTS_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}/api/products`
-// export const USERS_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}/api/users`
-// export const ORDERS_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}/api/orders`
-// export const PAYPAL_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}/api/config/paypal`
+export const BASE_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}`
+export const PRODUCTS_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}/api/products`
+export const USERS_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}/api/users`
+export const ORDERS_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}/api/orders`
+export const PAYPAL_URL = `${import.meta.env.VITE_APP_PROD_BACKEND_SERVER_BASE_URL}/api/config/paypal`
