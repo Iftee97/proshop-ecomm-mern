41d6130af0e7b183a6c7252c998af630889352bd
Iftee97 <tawsifmasrur@gmail.com>
2023-07-04 08:51:27 +0600

    ...

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,10 +6,23 @@ export const generateToken = (res, userId) => {
   });
 
   // Set JWT as an HTTP-Only cookie
+  // res.cookie('jwt', token, {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+  //   sameSite: 'strict', // Prevent CSRF attacks
+  //   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  // });
+
+  /**
+    sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
+    secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
+  */
+
+  // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
-    // httpOnly: false,
-    // secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
