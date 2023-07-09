d5f38abdee4b9e2cd4c322b1201ebce50d5fe56d
Iftee97 <tawsifmasrur@gmail.com>
2023-07-09 08:45:50 +0600

    fixed cookie issue

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,25 +4,6 @@ import User from '../models/userModel.js';
 
 // User must be authenticated
 const protect = asyncHandler(async (req, res, next) => {
-  // let token = req.cookies.jwt; // Read JWT from the 'jwt' cookie
-  // // console.log('token from middleware: >>>>>>>>', token);
-  // if (token) {
-  //   try {
-  //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  //     // console.log('decoded: >>>>>>>>', decoded);
-  //     req.user = await User.findById(decoded.userId).select('-password'); // exclude password
-  //     // console.log('req.user: >>>>>>>>', req.user);
-  //     next();
-  //   } catch (error) {
-  //     console.error(error);
-  //     res.status(401);
-  //     throw new Error('Not authorized, token failed');
-  //   }
-  // } else {
-  //   res.status(401);
-  //   throw new Error('Not authorized, no token');
-  // }
-
   const { authorization } = req.headers;
   // console.log('authorization: >>>>>>>>', authorization);
   if (!authorization) {
